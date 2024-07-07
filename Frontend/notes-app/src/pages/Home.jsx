ef4a115615ab5cc1cabaefbe9011f760408b5b55
radzhiv <radzhivkrishna@gmail.com>
2024-07-07 08:39:28 +0530

    feat. Base UI completed

--- Frontend/notes-app/src/pages/Home.jsx
+++ Frontend/notes-app/src/pages/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import NoteCard from "../components/NoteCard";
 
 export const Home = () => {
   return (
-    <div className=''>
-      
+    <div className="my-10 grid md:grid-cols-3 grid-cols-1 gap-5">
+      <NoteCard
+        title="hello"
+        date="4th July 2024"
+        content="Hello this is Rajeev, this is my MERN stack notes app"
+        tags="#Meeting"
+        isPinned={true}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        onPinNote={() => {}}
+      />
+       <NoteCard
+        title="hello"
+        date="4th July 2024"
+        content="Hello this is Rajeev, this is my MERN stack notes app"
+        tags="#Meeting"
+        isPinned={true}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        onPinNote={() => {}}
+      />
+       <NoteCard
+        title="hello"
+        date="4th July 2024"
+        content="Hello this is Rajeev, this is my MERN stack notes app"
+        tags="#Meeting"
+        isPinned={true}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        onPinNote={() => {}}
+      />
     </div>
-  )
-}
+  );
+};
