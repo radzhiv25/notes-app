f06c0b6e75df3517363dc5ab6c4690193e2c45cc
radzhiv <radzhivkrishna@gmail.com>
2024-07-07 08:39:02 +0530

    feat. Basic Navbar created

--- Frontend/notes-app/src/components/Navbar.jsx
+++ Frontend/notes-app/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Profile from "./Profile";
 import Search from "./Search";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
   const navigate = useNavigate;
 
+  const handleSearch = () => {};
+  const onClearSearch = () => {
+    setSearch("");
+  };
   const onLogout = () => {
     navigate("/login");
   };
@@ -14,14 +19,21 @@ const Navbar = () => {
     <div className="pt-5 py-2 border-b flex md:flex-row flex-col md:items-center justify-between ">
       <div className="flex md:flex-row flex-col md:items-center justify-between w-full gap-5 mr-5">
         <div className="">
-          <Link to="/home">
+          <Link to="/">
             <h1 className="text-3xl font-semibold text-slate-600">Notez</h1>
             <p className="text-xs text-gray-500">
               Simpligying your note taking
             </p>
           </Link>
         </div>
-        <Search />
+        <Search
+          value={search}
+          onChange={({ target }) => {
+            setSearch(target.value);
+          }}
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        />
       </div>
       <div className="md:mt-0 mt-2 flex items-center justify-between space-x-3">
         <ul className="flex space-x-3">
